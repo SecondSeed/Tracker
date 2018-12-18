5d785a9775ee392cf31ddd2d457e03858369aafb
SecondSeed <xxmhnu@163.com>
2018-12-18 14:32:05 +0800

    re commit

--- runTracker.m
+++ runTracker.m
@@ -14,8 +14,8 @@ params.enableopticalflow = 1;
 params.showflow = 0;
 
 %% load video info
-videoname = 'Woman'; 
-img_path = 'sequence/Woman/img/';
+videoname = 'Matrix'; 
+img_path = 'sequence/Matrix/img/';
 base_path = 'sequence/';
 [img_files, pos, target_sz, video_path] = load_video_info(base_path, videoname);
 
